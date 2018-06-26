16d9db8b066b9e25622b090bad75d05f6b31add9
Pham Thanh Phong <phongv25@gmail.com>
2018-06-26 08:36:23 +0700

    update giao diện cho index,thêm, sửa sách

---
+++ BiTech.Library/BiTech.Library/Scripts/main.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    $(".bt_menu").click(function () {
+        if ($(".contain").hasClass("small_menu")) {
+            $(".contain").removeClass("small_menu");
+        } else {
+            $(".contain").addClass("small_menu");
+        }
+    });
+});
