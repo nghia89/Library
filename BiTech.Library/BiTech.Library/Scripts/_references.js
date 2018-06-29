0b2868cb3b5a4251ee14f777ee0f1ef357cd1521
Thinh <thinh11577@gmail.com>
2018-06-29 08:30:37 +0700

    Cập nhật lại một số chỉnh sửa sau khi gộp code

--- BiTech.Library/BiTech.Library/Scripts/_references.js
+++ BiTech.Library/BiTech.Library/Scripts/_references.js
@@ -10,7 +10,6 @@
 /// <reference path="jquery.unobtrusive-ajax.min.js" />
 /// <reference path="jquery.validate.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
-/// <reference path="jquery-1.10.2.js" />
 /// <reference path="jquery-1.12.4.js" />
 /// <reference path="jquery-1.12.4.min.js" />
 /// <reference path="jquery-ui-1.12.1.js" />
