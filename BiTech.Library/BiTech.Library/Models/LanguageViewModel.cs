581e3de20a66e11b47c2950f5afc4acde4daa74c
Pham Thanh Phong <phongv25@gmail.com>
2018-09-19 14:05:36 +0700

    Sửa lỗi test 17-9

--- BiTech.Library/BiTech.Library/Models/LanguageViewModel.cs
+++ BiTech.Library/BiTech.Library/Models/LanguageViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,8 +10,12 @@ namespace BiTech.Library.Models
     {
         public string Id { get; set; }
 
+        [Required(ErrorMessage = "Vui lòng nhập tên ngôn ngữ")]
+        [Display(Name = "Ngôn ngữ")]
         public string Ten { get; set; }
 
+        [Required(ErrorMessage = "Vui lòng nhập tên tên ngắn")]
+        [Display(Name = "Tên ngắn")]
         public string TenNgan { get; set; }
     }
 }
