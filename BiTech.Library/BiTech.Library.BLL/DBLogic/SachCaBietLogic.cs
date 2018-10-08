81e877b96e61e0cceede6cd2eddb99c4fde8b2c0
Pham Thanh Phong <phongv25@gmail.com>
2018-10-08 11:21:49 +0700

    Update muonsach

---
+++ BiTech.Library/BiTech.Library.BLL/DBLogic/SachCaBietLogic.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BiTech.Library.DTO;
+using BiTech.Library.DAL.Engines;
+using BiTech.Library.DAL;
+
+namespace BiTech.Library.BLL.DBLogic
+{
+    public class SachCaBietLogic : BaseLogic
+    {
+        SachCaBietEngine _SachCaBietEngine;
+        public SachCaBietLogic(string connectionString, string databaseName)
+        {
+            Database database = new Database(connectionString);
+            _SachCaBietEngine = new SachCaBietEngine(database, databaseName, "SachCaBiet");
+        }
+
+        public List<SachCaBiet> GetAll()
+        {
+            return _SachCaBietEngine.GetAllSachCaBiet();
+        }
+
+        public string Add(SachCaBiet TL)
+        {
+            return _SachCaBietEngine.Insert(TL);
+        }
+        public SachCaBiet getById(string Id)
+        {
+            return _SachCaBietEngine.GetById(Id);
+        }
+        
+        public bool Update(SachCaBiet id)
+        {
+            return _SachCaBietEngine.Update(id);
+        }
+
+        public bool Delete(string Id)
+        {
+            return _SachCaBietEngine.Remove(Id);
+        }
+
+        #region Phong
+
+        public SachCaBiet GetAllByMaKSCBorMaCaBienCu(string masach)
+        {
+            return _SachCaBietEngine.GetAllByMaKSCBorMaCaBienCu(masach);
+        }
+
+        #endregion
+    }
+}
---
+++ BiTech.Library/BiTech.Library.DAL/Engines/SachCaBietEngine.cs
@@ -0,0 +1,34 @@
+﻿using BiTech.Library.DAL.Respository;
+using BiTech.Library.DTO;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiTech.Library.DAL.Engines
+{
+    public class SachCaBietEngine : EntityRepository<SachCaBiet>
+    {
+        public SachCaBietEngine(IDatabase database, string databaseName, string tableName) : base(database, databaseName, tableName)
+        {
+            _Database = (IMongoDatabase)database.GetConnection(databaseName);
+            _DatabaseCollection = _Database.GetCollection<SachCaBiet>(tableName);
+        }
+        #region Phong
+
+        public List<SachCaBiet> GetAllSachCaBiet()
+        {
+            return _DatabaseCollection.Find(_ => true).ToList();
+        }
+
+        public SachCaBiet GetAllByMaKSCBorMaCaBienCu(string idMaCaBiet)
+        {
+            return _DatabaseCollection.Find(_ => _.MaKSCB == idMaCaBiet || _.MaCaBienCu == idMaCaBiet).FirstOrDefault();
+        }
+
+        #endregion
+
+    }
+}
