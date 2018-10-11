a86b24833102a2626f194a6e721c6125a409bf47
Nghianguyen89 <hangnghia11089@gmail.com>
2018-10-11 14:05:09 +0700

    sửa một số lổi sau khi pull

---
+++ BiTech.Library/BiTech.Library.DAL/MongoMirgrations/BoSuutap_Mirgrations/Init.cs
@@ -0,0 +1,21 @@
+﻿using BiTech.Library.DTO;
+using Mongo.Migration.Migrations;
+using MongoDB.Bson;
+
+namespace BiTech.Library.DAL.MongoMirgrations.BoSuutap_Mirgrations
+{
+    public class Init : Migration<BoSuuTap>
+    {
+        public Init()
+           : base("0.0.1")
+        {
+        }
+        public override void Down(BsonDocument document)
+        {
+        }
+
+        public override void Up(BsonDocument document)
+        {
+        }
+    }
+}
